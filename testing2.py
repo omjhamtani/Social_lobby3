f110645757040760bd3d616cb2ab1ac2b718a124
Om Jhamtani <omjhamtani4@gmail.com>
2025-07-23 21:52:25 +0530

    Add files via upload

---
+++ testing2.py
@@ -0,0 +1,33 @@
+Last login: Wed Jul 23 20:32:02 on ttys000
+omjhamtani@Oms-MacBook-Air ~ % curl -i -X POST \
+  https://graph.facebook.com/v22.0/723041974226694/messages \
+  -H 'Authorization: Bearer EAAKqjj9PdSwBPFICO54NzYDZBp3cZBJSJAh4IUNZB3KmmiNu8WWaPgfZBRWcdM6FY6vv6FFkWlcwwPDcD8bKQmsJk8bdkgls3NbONcupMvk02oVFD2bTxWqaItqOJBvaRbAbu9o5hUTo10Ky0sHElmZBl9xXvNpukBrPsFuJhi6ZCVIOZCLeEH2pbuDoYZAxj8wpoaBRH4jH3UGg63I8yuTJg9bAEHZCbP8khmSYqmB7taBQZD' \
+  -H 'Content-Type: application/json' \
+  -d '{ "messaging_product": "whatsapp", "to": "919929596962", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
